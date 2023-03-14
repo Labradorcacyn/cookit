38c9661cb6f8fe226e19225df8bee649d8b318b2
Cynthia Labrador <cynthialabrador47@gmail.com>
2023-03-14 17:39:32 +0100

    Proyecto Java19 Spring Boot y Maven4

---
+++ cookit/src/main/java/com/project/cookit/CookitApplication.java
@@ -0,0 +1,15 @@
+package com.project.cookit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookitApplication.class, args);
+
+	}
+
+
+}
